f62dd8ab3329a74c0a6d21858af17778c8c1664f
mohammad taheri <mvtaheri@gmail.com>
2023-12-16 17:39:24 +0330

    update project

---
+++ app/src/main/java/com/vahid/drageanddrop/PersonUiItem.kt
@@ -0,0 +1,9 @@
+package com.vahid.drageanddrop
+
+import androidx.compose.ui.graphics.Color
+
+data class PersonUiItem(
+    val name: String,
+    val id: String,
+    val backgroundColor: Color
+)
